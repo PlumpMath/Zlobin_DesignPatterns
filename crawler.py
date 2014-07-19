11253728d0d2a1ca60778dc2eee0a3d6236705cf
root <root@uf8bc1277c04e53adda5e.ant.amazon.com>
2014-07-18 17:47:17 -0700

    Initial commit

---
+++ crawler.py
@@ -0,0 +1,22 @@
+__author__ = 'root'
+
+import httplib2
+import os
+import re
+import threading
+import urllib
+from urlparse import urlparse, urljoin
+from BeautifulSoup import BeautifulSoup
+
+class Singleton(object):
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Singleton, cls).__new__(cls)
+        return cls.instance
+
+x = Singleton()
+y = Singleton()
+
+print x is y
+
+
